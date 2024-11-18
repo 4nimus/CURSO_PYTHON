17dae2b803d209693c3fc75f6c1531b31b3e3a11
Alexander Matute <alexjosematute@gmail.com>
2024-11-18 13:26:10 -0400

    Create tipos_datos.py

---
+++ module 2/tipos_datos.py
@@ -0,0 +1,22 @@
+#Ejemplo de tipos de datos en python
+
+#entero
+edad = 30 
+print('Edad:', edad)
+
+#Numero con punto flotante
+altura = 1.75
+print('Altura:', altura)
+
+#Cadena de texto
+nombre = 'Juan'
+print('Nombre:', nombre)
+
+# Tipo boolean
+es_estudiante = False
+print('Es estudiante?:', es_estudiante)
+
+#none, ausencia de valor
+
+direccion = None
+print('Dirección:', direccion)
