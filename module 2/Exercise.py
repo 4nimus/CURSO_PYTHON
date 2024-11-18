668a897fccd662e32d3d448c6c0bde62e58f7289
Alexander Matute <alexjosematute@gmail.com>
2024-11-18 14:37:56 -0400

    Create Exercise.py

---
+++ module 2/Exercise.py
@@ -0,0 +1,18 @@
+#Crea un sistema de reserva de hoteles
+#que contenga la siguiente información
+#de una reserva:
+#Nombre del cliente
+#Dias de estancia
+#tarifa diaria
+#indicar si el cuarto tiene vista al mar
+#Despues mandar a imprimir los valores de cada variable
+#hacer algunos cambios y reimprimir
+cliente = 'Laura Martinez'
+dias_estancia = 5
+tarifa_diaria = 1200.0
+vista_mar = True
+print('***Sistema de Reserva de Hoteles***')
+print('Nombre del cliente:', cliente)
+print('Dias de estancia:', dias_estancia)
+print('Tarifa diaria', tarifa_diaria)
+print('Vista al mar?:', vista_mar)
