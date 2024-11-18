855cd1f1d8a590df978196f865e7110cabb97cda
Alexander Matute <alexjosematute@gmail.com>
2024-11-18 10:03:37 -0400

    Create variables.py

---
+++ module 2/Elementos_basicos_variables.py
@@ -0,0 +1,31 @@
+# Variables en python
+
+
+# Declaración e inicialización de variables
+edad = 28
+altura = 1.65
+pais = 'Mexico'
+
+#Acceder a las variables
+print('Valores inciales:')
+print('Edad:',edad)
+print('Altura:',altura)
+print('Pais:',pais)
+
+#Modificar el valor de una variable
+edad = 30
+altura = 1.68
+
+#Acceder a las variables
+print('Valores modificados:')
+print('Edad:',edad)
+print('Altura:',altura)
+print('Pais:',pais)
+
+#En python el tipo es dinamico
+edad = 'treinta'
+print('Edad:',edad) 
+
+#Si queremos acceder a una variable no declarada va a dar error
+telefono = '5544663388'
+print('Telefono:', telefono)
