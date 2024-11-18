d15d57aa0f8ec89c0c70da6adff95ad93af27bd7
Alexander Matute <alexjosematute@gmail.com>
2024-11-18 15:07:28 -0400

    Create Exercise2.py

---
+++ module 2/Exercise2.py
@@ -0,0 +1,16 @@
+#Crear el detalle de un producto de una tienda online
+#El detalle del producto debe tener
+#Nombre del producto
+#Precio del producto
+#Cantidad en el inventario
+#indicar si esta disponible
+producto = 'Cámara digital'
+precio = 399.9
+inventario = 20
+disponible = True
+
+print('*** Sistema de Tienda Online ***')
+print('Producto:',producto)
+print('Precio: $',precio)
+print('Cantidad en el Inventario:',inventario)
+print('Disponible para entrega:',disponible)
