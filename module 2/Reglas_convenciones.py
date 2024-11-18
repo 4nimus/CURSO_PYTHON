97a52502658f66adcc683e221f8aec0adbfd4491
Alexander Matute <alexjosematute@gmail.com>
2024-11-18 10:34:17 -0400

    Create Reglas_convenciones.py

---
+++ module 2/Reglas_convenciones.py
@@ -0,0 +1,21 @@
+#Reglas y convenciones en nombres de variables
+
+#Ejemplos de reglas estrictas
+nombre_usuario = 'Juan Perez'
+
+#No podemos usar palabras reservadas
+#class = 'mi clase'
+klass = 'Mi clase'
+
+#Sensibles a mayusculas y minusculas
+nombre = 'Juan'
+NOMBRE = 'Karla'
+print(nombre)
+print(NOMBRE)
+
+#Snake case
+nombre_completo = 'Ricardo Esparza'
+
+# prefijos y sufijos
+es_casado = True
+nombre_txt = 'archivos.txt'
