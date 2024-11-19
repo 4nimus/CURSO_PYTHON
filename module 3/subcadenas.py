f929e7642d53ebeb8d1df6d164fe40a30d7b4713
Alexander Matute <alexjosematute@gmail.com>
2024-11-19 15:10:19 -0400

    Create subcadenas.py

---
+++ module 3/subcadenas.py
@@ -0,0 +1,9 @@
+#Manejo de subCadenas
+cadena1 = 'hola mundo'
+
+#Obtenemos la subcadena de hola [inicio:fin (sin incluirlo)]
+subcadena_hola = cadena1[0:4]
+print(f'Subcadena de hola: {subcadena_hola}')
+
+subcadena_mundo = cadena1[5:10]
+print(f'Subcadena de mundo: {subcadena_mundo}')
