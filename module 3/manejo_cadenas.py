18302972893f4b03eed9370ae2d52bc91020db38
Alexander Matute <alexjosematute@gmail.com>
2024-11-19 08:43:34 -0400

    Create manejo_cadenas.py

---
+++ module 3/manejo_cadenas.py
@@ -0,0 +1,11 @@
+#Cadenas en Python
+cadena1 = 'hola mundo'
+cadena1 = 'adios'
+cadena2 = "Python es genial"
+Cadena3 = ''' este es un ejemplo
+de multiples lineas
+en una cadena '''
+
+print(cadena1)
+print(cadena2)
+print(Cadena3)
