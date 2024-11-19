62d0f4e404bdd3973c65b9316f31742e4bf8e972
Alexander Matute <alexjosematute@gmail.com>
2024-11-19 09:01:38 -0400

    Create indices_cadenas.py

---
+++ module 3/indices_cadenas.py
@@ -0,0 +1,9 @@
+#Manejo de indice en una cadena
+cadena1 = 'hello world'
+print(cadena1)
+
+#Recuperar el primer caracter
+primer_caracter = cadena1[0] #Recupera 'h'
+print(primer_caracter)
+ultimo_caracter = cadena1[10]
+print(ultimo_caracter) #Recupera 'd'
