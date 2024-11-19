a95a1ace27d068c60dd6ebc69b97fa3a0564c09a
Alexander Matute <alexjosematute@gmail.com>
2024-11-19 15:27:09 -0400

    Create reemplazar_subcadenas.py

---
+++ module 3/reemplazar_subcadenas.py
@@ -0,0 +1,10 @@
+#Reemplazar subcadenas
+
+cadena = 'Hola, Mundo!'
+print(f'Cadena original: {cadena}')
+nueva_cadena = cadena.replace('Mundo', 'a todos')
+print(f'Nueva cadena reemplazada: {nueva_cadena}')  # Salida: Hola, a todos!
+
+#sustituir hola por adios
+nueva_cadena = cadena.replace('Hola', 'adios')
+print(f'Nueva cadena reemplazada: {nueva_cadena}')  # Salidad: adios, mundo
