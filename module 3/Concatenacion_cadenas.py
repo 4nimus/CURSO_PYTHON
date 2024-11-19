26d315f40ab777ae598dd4a720038e08876deea6
Alexander Matute <alexjosematute@gmail.com>
2024-11-19 09:35:00 -0400

    Create Concatenacion_cadenas.py

---
+++ module 3/Concatenacion_cadenas.py
@@ -0,0 +1,10 @@
+#concatenación de Candenas
+cadena1 = 'hello'
+cadena2 = 'world'
+concatenacion = cadena1 +' '+ cadena2
+print(concatenacion)  # Salida: helloworld
+
+#Metodo JOIN
+concatenacion = ''.join([cadena1, ' ', cadena2])
+print(concatenacion)
+
