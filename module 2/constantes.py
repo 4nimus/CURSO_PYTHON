38558ffe02e9738a0408b4ed48ea30f9303e4091
Alexander Matute <alexjosematute@gmail.com>
2024-11-19 08:34:00 -0400

    Create constantes.py

---
+++ module 2/constantes.py
@@ -0,0 +1,8 @@
+print ('*** Constantes en Python ***')
+
+PI = 3.14159
+print ('El valor de PI es:',PI)
+
+NOMBRE_BASE_DATOS = 'clientes_db'
+print ('El nombre de la base de datos es:', NOMBRE_BASE_DATOS)
+
