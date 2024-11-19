cc886e5ef3cebd39bfa210d20d46739d08cfefb6
Alexander Matute <alexjosematute@gmail.com>
2024-11-19 11:04:37 -0400

    Create formateo_cadenas.py

---
+++ module 3/formateo_cadenas.py
@@ -0,0 +1,12 @@
+#Formateo de Cadenas
+
+nombre = 'Juan'
+edad = 25
+
+# f-sting
+mensaje = f'Hello, i am name {nombre} and have {edad} year.'
+print(mensaje)
+
+#Metodo format
+mensaje = 'Hello, i am name {} and have {} year.'.format(nombre, edad)
+print(mensaje)
