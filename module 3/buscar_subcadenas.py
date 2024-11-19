b7bbec4a1e5610a7f2cc17c73140cd779f96ee14
Alexander Matute <alexjosematute@gmail.com>
2024-11-19 15:17:51 -0400

    Create buscar_subcadenas.py

---
+++ module 3/buscar_subcadenas.py
@@ -0,0 +1,9 @@
+#Buscar subcadenas
+
+cadenas = 'Hola, Mundo!'
+indice = cadenas.find('Mundo')
+print(f'Indice de la subcadena mundo: {indice}')  # Salida: 6
+
+#Obtener el indice de la subcadena de hola
+indice = cadenas.find('Hola')
+print(f'Indice de la subcadena hola: {indice}')  # Salida:
