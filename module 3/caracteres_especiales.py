747b1080c5f2a78c7704e2b00f7365e70a5033bd
Alexander Matute <alexjosematute@gmail.com>
2024-11-19 09:22:09 -0400

    Create caracteres_especiales.py

---
+++ module 3/caracteres_especiales.py
@@ -0,0 +1,6 @@
+#Caracteres Especiales
+print('hola \nMundo') #\n salto de linea
+print('\tpython es Genial') #\t agrega un tabuldar
+print('Juan\' Perez')
+print("Karla \" Gomez")
+print('Caracter \\ diagonal invertidad')
