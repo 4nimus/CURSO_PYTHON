b4fe352e618df929bf18ded2d0cb2ca34def5d8d
Alexander Matute <alexjosematute@gmail.com>
2024-11-19 09:27:44 -0400

    Update caracteres_especiales.py

--- module 3/caracteres_especiales.py
+++ module 3/caracteres_especiales.py
@@ -4,3 +4,4 @@ print('\tpython es Genial') #\t agrega un tabuldar
 print('Juan\' Perez')
 print("Karla \" Gomez")
 print('Caracter \\ diagonal invertidad')
+#Comillas dobles o simples pueden ser usadas para crear cadenas de texto en Python. 
