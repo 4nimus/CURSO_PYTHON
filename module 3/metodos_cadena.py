5c5d03eaa993b80c0dd9b085bea44821499c44c8
Alexander Matute <alexjosematute@gmail.com>
2024-11-19 11:31:37 -0400

    Create metodos_cadena.py

---
+++ module 3/metodos_cadena.py
@@ -0,0 +1,12 @@
+#Metodos de cadenas
+
+cadena1 = 'Hola Mundo'
+print(f'Cadena orginal: {cadena1}')
+mayusculas = cadena1.upper()
+print(f'Cadena en mayusculas: {mayusculas}') #convertir a mayusculas
+minisculas = cadena1.lower()
+print(f'Cadena en minusculas: {minisculas}') #convertir a minusculas
+cadena2 = ' Juan perez'
+print(f'Cadena con espacios: {cadena2}')
+print(f'Cadena sin espacios: {cadena2.strip()}') #Eliminar espacios al incio y al final
+
