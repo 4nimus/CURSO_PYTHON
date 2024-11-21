599aa204c1d4e4e1095d0daad9c546912178d306
Alexander Matute <alexjosematute@gmail.com>
2024-11-20 21:09:41 -0400

    Create separar_cadenas.py

---
+++ module 3/separar_cadenas.py
@@ -0,0 +1,10 @@
+#Separar cadenas (split)
+
+datos = 'Hello World'
+lista = datos.split() #Por default separa cada elemento por espaciós en blanco
+
+print(lista)
+
+datos = 'Juan,30,Mexico'
+lista = datos.split(',')
+print(lista)
