2a4498fdbbff6c98f45e2c36a21b6c59bc38bf3d
Alexander Matute <alexjosematute@gmail.com>
2024-11-30 18:21:30 -0400

    Create operadores_condicionales.py

---
+++ module 5/operadores_condicionales.py
@@ -0,0 +1,25 @@
+print(f'*** Operadores compración (Relacionales)***')
+a,b = 7,5
+print(f'Valor incial a: {a}, b: {b}')
+
+#operador igualdad ==
+resultado = a == b
+print(f' a == b: {resultado}')
+
+#operador desigualdad !=
+resultado = a != b
+print(f' {a} != {b}: {resultado}')
+#operador mayor que >
+resultado = a > b
+print(f' {a} > {b}: {resultado}')
+#operador menor que <
+resultado = a < b
+print(f' {a} < {b}: {resultado}')
+#operador mayor o igual que >=
+resultado = a >= b
+print(f' {a} >= {b}: {resultado}')
+#operador menor o igual que <=
+resultado = a <= b
+print(f' {a} <= {b}: {resultado}')
+#operador igualdad estrita ===
+
