ededb18a264fb3ffbfde625d1fade6a7a6260369
Alexander Matute <alexjosematute@gmail.com>
2024-11-30 17:32:16 -0400

    Create operadores_compuesto.py

---
+++ module 5/operadores_compuesto.py
@@ -0,0 +1,42 @@
+#Operadores compuesto
+
+print(f'*** Operadores Asignación compuestos ***')
+a,b = 10,15
+print(f'Valor de a: {a} y valor de b: {b}')
+
+#operador compuesto de suma +=
+print(f'*** Operador suma += ***')
+a += b
+print(f'Valor de a: {a}')
+
+#operador compuesto de resta -=
+print(f'*** Operador resta -= ***')
+a -= b
+print(f'Valor de a: {a}')
+
+#operador compuesto de multiplicación *=
+print(f'*** Operador multiplicación *= ***')
+a *= b
+print(f'Valor de a: {a}')
+
+#operador compuesto de división /=
+print(f'*** Operador división /= ***')
+a /= b
+print(f'Valor de a: {a}')
+
+#operador compuesto de potencia **=
+print(f'*** Operador potencia **= ***')
+a **= b
+print(f'Valor de a: {a}')
+
+#operador compuesto de módulo %=
+print(f'*** Operador módulo %= ***')
+a %= b
+print(f'Valor de a: {a}')
+
+#operador compuesto de división entera //=
+print(f'*** Operador división entera //= ***')
+a //= b
+print(f'Valor de a: {a}')
+
+
