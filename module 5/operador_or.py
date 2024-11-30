bdd0df41ffe13a0346f06e26f49b4d35751a09f9
Alexander Matute <alexjosematute@gmail.com>
2024-11-30 19:22:00 -0400

    Create operador_or.py

---
+++ module 5/operador_or.py
@@ -0,0 +1,5 @@
+print(f'*** Operador logico or ***')
+condicion1, condicion2 = False, True
+#El operador or regresa true si cualquiera de los operandos es true
+resultado = condicion1 or condicion2
+print(f'El resultado {condicion1} or {condicion2} es: {resultado}')
