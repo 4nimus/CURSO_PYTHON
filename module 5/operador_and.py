246314852b31a3daa6f1a1fd8626e9b0231d32d5
Alexander Matute <alexjosematute@gmail.com>
2024-11-30 19:00:55 -0400

    Create operador_and.py

---
+++ module 5/operador_and.py
@@ -0,0 +1,5 @@
+print('*** Operador and***')
+#Regresa verdadero si ambos valores a evaluar son verdaderos
+condicion1, condicion2 = True, True
+resultado = condicion1 and condicion2
+print(f'Resultado { condicion1 } and {condicion2}: {resultado}')
