d2cd358f9b3d795cf1cc9fce00f113742bbc8d2f
Alexander Matute <alexjosematute@gmail.com>
2024-12-07 14:00:16 -0400

    Create valor_positivo.py

---
+++ module 6/valor_positivo.py
@@ -0,0 +1,10 @@
+print("**** Revisión valor Positivo ****")
+
+numero = int(input("Proporciona un numero: "))
+
+if numero > 0:
+    print("El numero es positivo")
+elif numero < 0:
+    print("El numero es negativo")
+else:
+    print("El numero es cero")
