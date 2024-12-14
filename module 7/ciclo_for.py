81f142518a4ffc791f7276faa58f050a54f93668
Alexander Matute <49373985+4nimus@users.noreply.github.com>
2024-12-14 15:37:34 -0400

    Create ciclo_for.py

---
+++ module 7/ciclo_for.py
@@ -0,0 +1,13 @@
+print('**** Ciclo FOR ****')
+
+cadena = 'hello worl'
+
+#iteramos los caracteres
+
+for letra in cadena:
+    print(letra, end= ' - ' )
+
+print('\n\n Recorremos la lista de frutas')
+frutas = ['Platano', 'Fresa', 'Mango']
+for frutas in frutas:
+    print(frutas, end= ' ')
