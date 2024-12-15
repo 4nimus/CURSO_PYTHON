58d6c3713fe24719556edce45f0a4d4f6ea81cb4
Alexander Matute <49373985+4nimus@users.noreply.github.com>
2024-12-15 15:01:56 -0400

    Create validacion_campo_formulario.py

---
+++ module 7/validacion_campo_formulario.py
@@ -0,0 +1,8 @@
+print('****** Validacion campo de un formulario******')
+
+nombre_usuario = None
+
+while not nombre_usuario:
+    nombre_usuario = input('Digite su nombre de usuario: ')
+
+print(f'Nombre de usuario valido: {nombre_usuario}')
