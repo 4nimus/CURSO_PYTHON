2dcf3b1adee49b6bec29a1d1f20f891bae77d71e
Alexander Matute <alexjosematute@gmail.com>
2024-12-18 13:13:01 -0400

    Update diccionarios.py

--- module 8/diccionarios.py
+++ module 8/diccionarios.py
@@ -11,3 +11,29 @@ print(f'NOMBRE: {persona['nombre']}')
 print(f'EDAD: {persona.get('edad')}')
 print(f'CIUDAD: {persona.get('ciudad')}')
 
+#MODIFICAR UN VALOR DEL DICCIONARIO
+persona['edad'] = 15
+print(f'DICCIONARIO DE PERSONA: {persona.get('edad')}')
+
+#AGREGAR UN NUEVO ELEMENTO
+persona['profesion'] = 'ingeniero'
+print(f'DICCIONARIO DE PERSONA: {persona.get('profesion')}')
+
+#ELIMINAR UN ELEMENTO
+del persona['ciudad']
+print(f'DICCIONARIO DE PERSONA: {persona}')
+
+persona.pop('profesion')
+print(f'DICCIONARIO DE PERSONA: {persona}')
+
+#iterar los elemntos de un dict (llave, valor)
+for llave, valor in persona.items():
+    print(f'LLAVE: {llave}, valor: {valor}')
+print('\n')
+#obtener los valores
+for valor in persona.values():
+    print(f'VALOR: {valor}')
+print('\n')
+#obtener las llaves
+for llave in persona.keys():
+    print(f'LLAVE: {llave}')
