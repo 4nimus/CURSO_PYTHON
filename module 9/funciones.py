ecec736b88b5224f2b92402c1ecad368d4317115
Alexander Matute <alexjosematute@gmail.com>
2024-12-24 11:16:09 -0400

    Create funciones.py

---
+++ module 9/funciones.py
@@ -0,0 +1,12 @@
+print(' *** Funcones en python *** ')
+
+#definir una funcion para mandar a saludar
+def saludar(nombre):
+    print(f"Hola {nombre} como estas?")
+
+#llamar a la funcion
+saludar("Juan")
+saludar(2)
+saludar(True)
+saludar(None)
+
