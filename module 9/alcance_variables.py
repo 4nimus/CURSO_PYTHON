7cb471c1a1d125b60f61ac9aef428f7603c0ab6d
Alexander Matute <49373985+4nimus@users.noreply.github.com>
2024-12-29 08:16:25 -0400

    Update alcance_variables.py

--- module 9/alcance_variables.py
+++ module 9/alcance_variables.py
@@ -6,13 +6,20 @@ contador_global = 0
 def incrementar_contador():
     #declaramos una variable local
     contador_local = 0
-
     #usar la variable global
     global contador_global
-    contador_global += 1
-    
+    contador_global += 1 
     #Incrementar la variable local
     contador_local += 1
-
     #imprimir ambos contadores
-    print(f'')
+    print(f'Contador LOCAL: {contador_local}')
+    print(f'Contador Global: {contador_global}\n')
+
+#Llamamos varias vece la función
+incrementar_contador()
+incrementar_contador()
+incrementar_contador()
+
+#terminando el programa
+print(f'Valor variable global: {contador_global}')
+
