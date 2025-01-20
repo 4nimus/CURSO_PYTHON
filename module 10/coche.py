f22980ec3e3f4f09c42eba6bcf7c76a73db2f98c
Alexander Matute <49373985+4nimus@users.noreply.github.com>
2025-01-20 11:01:45 -0400

    Update coche.py

--- module 10/coche.py
+++ module 10/coche.py
@@ -12,22 +12,36 @@ class Coche:
               Modelo: {self._modelo}
               Color: {self._color}''')
     
-    def get_marca(self):
+    #   def get_marca(self):
+    #    return self._marca
+    #    def get_modelo(self):
+    #    return self._modelo
+    #     def get_color(self):
+    #   return self._color
+    
+
+    @property 
+    def marca(self):
         return self._marca
     
-    def set_marca(self, marca):
-        self._marca = marca
-
-    def get_modelo(self):
+    @property
+    def modelo(self):
         return self._modelo
     
-    def set_modelo(self, modelo):
-        self._modelo = modelo
-    
-    def get_color(self):
+    @property
+    def color(self):
         return self._color
     
-    def set_color(self, color):
+    @marca.setter
+    def marca(self, marca):
+        self._marca = marca
+
+    @modelo.setter
+    def modelo(self, modelo):
+        self._modelo = modelo
+    
+    @color.setter
+    def color(self, color):
         self._color = color
     
 
@@ -37,7 +51,15 @@ if __name__== '__main__':
     coche1 = Coche('toyota','yaris','azul')
     coche1.conducir()
     #No deberiamos acceder a los atributos que no sean publicos
-    coche1.set_marca('Toyota2')
-    coche1.set_modelo('Yaris') #esto no es una buena practica
-    coche1.set_color('rojo') #Ignoro la modificaicón
-    coche1.conducir()
+    #coche1.set_marca('Toyota2')
+    #coche1.set_modelo('Yaris') #esto no es una buena practica
+    #coche1.set_color('rojo') #Ignoro la modificaicón
+    #coche1.conducir()
+
+#Atributo de marca del coche 1
+print(f'Atributo marca coch1: {coche1.marca}')
+coche1.marca = 'Toyota 3'
+coche1.modelo = 'YARIS'
+coche1.color = 'amarillo'
+print(f'Atributo marca coche1: {coche1.marca}')
+coche1.conducir()
