1eba4ea9593ed612fd38342cc3209396891c2bca
Alexander Matute <alexjosematute@gmail.com>
2025-01-28 15:37:10 -0400

    CURSO

--- MENU CARLOS BARBOZA A&P MIERCOLES 10  12.py
+++ MENU CARLOS BARBOZA A&P MIERCOLES 10  12.py
@@ -3,34 +3,82 @@
 #SE SALE DE FORMA ABRUPTA
 #NO SE INFORMA SOBRE OPCIÓN INCORRECTA
 
+##################################################################################################################
+#LIBRERIAS
+##################################################################################################################
+
 import os
+import json
 import re
+import sys
 
-def crear_registro_txt(nombre_archivo,registro):
+##################################################################################################################
+#VALIDA O CREA EL TXT
+##################################################################################################################
 
-        
-        escritura = open(nombre_archivo, 'a')
-        escritura.writelines('\n'+registro )
-        escritura.close()
-
-
-def crear_txt():
-    
-    nombre_archivo= 'REGISTROS.txt'
+def crear_txt(nombre_archivo):
 
     a = os.path.isfile(nombre_archivo)
     
-    if os.path.exists(a) == True:
-        #print('Ya existe')
-        registro = input(f'Registro 1: ')
-        return crear_registro_txt(nombre_archivo, registro) 
+    if os.path.exists(nombre_archivo) == True:
+        print('Ya existe')
 
-    elif os.path.exists(a) == False:
+    elif os.path.exists(nombre_archivo) == False:
         file = open(nombre_archivo, 'x')
 
-#OTRAS OPCIONES
+#################################################################################################################
+#CREA EL REGISTRO A GUARDAR EN EL TXT
+#################################################################################################################
+
+def crear_registro_txt(nombre_archivo,registro):
+
+        with open(nombre_archivo, 'a') as escritura:
+            json.dump(registro, escritura, indent=7)
+
+#################################################################################################################
+#AGREGAR REGISTROS A LOS TXT
+#################################################################################################################
+
+def agregar_registro(nombre_archivo):
+    while True:
+        seguir_agregando = input(f'Quieres crear un registro nuevo en el ARCHIVO: {nombre_archivo} SI/NO?: ').lower().strip()
+        registro = {}
+        if seguir_agregando == 'si':
+                id = int(input('Digite el codigo del producto: '))
+                nombre = input('Digite nombre del producto: ').lower().strip()
+                precio = float(input('Digite precio del producto: '))
+                cantidad = int(input('Digite cantidad del producto: '))
+                registro[id] = {
+                    'nombre': nombre,
+                    'precio': precio,
+                    'cantidad': cantidad
+                }
+                crear_registro_txt(nombre_archivo,registro)
+
+        elif seguir_agregando == 'no':
+            print('VOLVIENDO AL MENU PRINCIPAL')
+            break
+        else:
+            print('INGRESE UNA OPCION VALIDA')
+
+##################################################################################################################
+#SALIDA DE OPCINES
+##################################################################################################################
+
 def salir_opciones_menu(opcion):
     input(f'PRECIONAR ENTER PARA SALIR DE LA OPCION {opcion}')
+    exit()
+
+##################################################################################################################
+
+def usar(contenido, txt):
+    for contenidos in contenido:
+        if contenidos.endswith(".txt") and contenidos == txt:
+            agregar_registro(contenidos)  
+
+##############################################################################################################
+#ESTRUCTURA DE MENU
+##################################################################################################################
 
 def MENU():
     print("\t\tMENU CARLOS BARBOZA A&P MIERCOLES 10:00\n")
@@ -41,34 +89,100 @@ def MENU():
     print("5. DESINCORPORAR")
     print("0. SALIR\n")
 
-#OM VARIABLE PARA IDENTIFICAR OPCION DE MENU
-def OM1():
-    print("\n*****REGISTRO*******\n")
-    crear_txt()
+##################################################################################################################
+#MENU PRINCIPAL
+##################################################################################################################
 
-    salir_opciones_menu(seleccion)
+##################################################################################################################
+#REGISTRO
+##################################################################################################################
+
+def OM1():
+
+    while True:
+        print("\n*****REGISTRO*******\n")
+                
+        contenido = os.listdir()
+        print('ARCHIVOS ENCONTRADOS: \n')
+        n = 0
+        for contenidos in contenido:
+            if contenidos.endswith(".txt"):
+                n += 1
+                print(f'{n}) {contenidos}\n')
+        crear_usar = input(f'USAR UN ARCHIVO EXISTENTE O USAR UNO NUEVO "CREAR/USAR" DE LO CONTRARIO PRECIONE ENTER PARA SALIR?: ').lower().strip()
+
+        if crear_usar == 'crear':
+            nombre_archivo = input('INGRESE EL NOMBRE DEL ARCHIVO A CREAR: ').lower().strip()
+            txt = f'{nombre_archivo}.txt'
+            crear_txt(txt)
+            agregar_registro(txt)
+       
+        elif crear_usar == 'usar':
+            salir = 'no'
+            while True:
+                if salir.lower().strip() == 'si':
+                    break
+                elif salir.lower().strip() == 'no':
+                    print(f'\nSeguir eligiendo el archivo\n')
+                    archivo = input('INGRESE EL NOMBRE DEL ARCHIVO: ').lower().strip()
+                    txt = (f'{archivo}.txt').lower().strip()
+                    usar(contenido, txt)
+                else:
+                    print(f'\nOpción Invalida\n')
+                salir = input(f'QUIERES SALIR DE ESTA OPCIÓN SI/NO: ').lower().strip()
+
+        elif crear_usar == '':
+            break
+
+
+        else:
+            print('INGRESE UNA OPCION VALIDA')
+
+
+####################################################################################################################
+#CONSULTA
+##################################################################################################################
 
 def OM2():
     print("\n*******CONSULTA*******\n")
     salir_opciones_menu(seleccion)
 
+##################################################################################################################
+#LISTADO
+##################################################################################################################
+
 def OM3():
     print("\n*******LISTADO*******\n")
     salir_opciones_menu(seleccion)
+
+##################################################################################################################
+#MODIFICAR
+##################################################################################################################
 
 def OM4():
     print("\n*******MODIFICAR*******\n")
     salir_opciones_menu(seleccion)
 
+##################################################################################################################
+#DESINCORPORAR
+##################################################################################################################
+
 def OM5():
     print("\n*******ESINCORPORAR*******\n")
     salir_opciones_menu(seleccion)
 
+##################################################################################################################
+#SALIR
+##################################################################################################################
+
 def OM0():
     print("SALIR")
-    
 
-while True:  #INCORRECTO
+##################################################################################################################
+#MENU PRINCIPAL FUNCIONAL
+##################################################################################################################
+
+while True:
     MENU()
     seleccion = input("SELECCIONE UNA OPCION: ")
 
