c5aab69ea25771a048a0148be68365aabf32fa60
Alexander Matute <49373985+4nimus@users.noreply.github.com>
2025-02-02 15:41:37 -0400

    Update catalogo_peliculas.py

--- Modulo basico/laboratorio_final/servicio/catalogo_peliculas.py
+++ Modulo basico/laboratorio_final/servicio/catalogo_peliculas.py
@@ -3,12 +3,15 @@ class CatalogoPelicula(Pelicula):
 
     def CatalogoPeliculas(self, ruta_archivo):
         self.ruta_archivo = ruta_archivo
-    
-    def agregar_pelicula(self):
-        pass
+        
+    def agregar_pelicula(self, archivo, pelicula):
+            contador = None
+            contador += 1
+            archivo.write(f'{contador}) {Pelicula(pelicula)}\n')
 
-    def listar_peliculas(self):
-        pass
+    def listar_peliculas(self,archivo):
+        for linea in archivo:
+            print(linea)
     
     def eliminar(self):
         pass
